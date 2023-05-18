38eb00d5d7a6257e1e7402f63e14881b0a2da687
Hikaru-q <132335482+Hikaru-q@users.noreply.github.com>
2023-05-18 13:09:27 +0000

    Bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,10 +15,17 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
   end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end  
   
   
   private
