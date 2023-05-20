c1d6437e3c61858a7ce21e8d09d2e22312d7a05b
Hikaru-q <132335482+Hikaru-q@users.noreply.github.com>
2023-05-20 14:06:00 +0000

    Bookers2

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update]
   get "/homes/about" => "homes#about", as: "about"
 end
