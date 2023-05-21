305c5948d8e098ae1740b2b053ced80b0f56f20e
Hikaru-q <132335482+Hikaru-q@users.noreply.github.com>
2023-05-21 15:59:28 +0000

    Bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -33,17 +33,6 @@ class BooksController < ApplicationController
         redirect_to books_path
     end
   end
-
-  def update
-    @book = Book.find(params[:id])
-    @book.user_id = current_user.id
-    if @book.update(book_params)
-      flash[:notice]="Book was successfully updated."
-      redirect_to book_path(@book.id)
-    else
-      render :edit
-    end
-  end
   
   def destroy
     book = Book.find(params[:id])
